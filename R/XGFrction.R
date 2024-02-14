a15001715cbc01a79bb304f76a909f5a859ada1c
dbergl1344 <132304482+dbergl1344@users.noreply.github.com>
2024-02-14 10:06:56 -0700

    Update XGFrction.R

--- R/XGFrction.R
+++ R/XGFrction.R
@@ -1,61 +1,13 @@
-library(devtools)
-
-#' Calculate friction
-#'
-#' This function calculates friction based on the given parameters.
-#'
-#' @param velocity Numeric vector of velocities.
-#' @param coefficient Numeric vector of friction coefficients.
-#'
-#' @return Numeric vector of calculated friction.
-#'
-#' @examples
-#' velocity <- c(10, 20, 30)
-#' coefficient <- 0.5
-#' calculate_friction(velocity, coefficient)
-calculate_friction <- function(velocity, coefficient) {
-  friction <- velocity * coefficient
-  return(friction)
-}
-
-
-
-
-# NAMESPACE file content
-exportPattern("^[[:alpha:]]+")
-
-# Add this at the beginning of friction_function.R
-#' @importFrom stats sd
-#' @export
-
-
-
-
-
-# Run these in your R console
-install.packages("devtools")
-devtools::install_deps()
-
-
-
-
-# Run these in your R console
-devtools::document()
-devtools::install()
-
-
-
-
 #' Friction Velocity Filtering
 #'
 #' This function performs friction velocity filtering on the input data.
 #'
-#' @param data The input data frame containing CO2 flux and other relevant variables.
+#' @param data A data frame containing CO2 flux and other relevant variables.
 #' @return A data frame with filtered data.
-#' @export
 #' @examples
 #' data <- readr::read_csv("your_data.csv")
 #' filtered_data <- friction_velocity_filter(data)
+#' @export
 friction_velocity_filter <- function(data) {
   # Implement your friction velocity filtering logic
   # ...
@@ -63,68 +15,65 @@ friction_velocity_filter <- function(data) {
   return(filtered_data)
 }
 
-
-
-
-
-#' Bootstrap Ustar Thresholds
+#' Convert Timestamps to POSIX
 #'
-#' This function performs bootstrapping to determine Ustar thresholds for different years.
+#' This function converts different time formats to POSIX format.
 #'
-#' @param data The input data frame containing CO2 flux and other relevant variables.
+#' @param data Data frame with time columns to be converted.
+#' @param TFormat Abbreviation for implemented time formats.
+#' @param Year Column name of year.
+#' @param Month Column name of month.
+#' @param Day Column name of day.
+#' @param Hour Column name of hour.
+#' @param Min Column name of minute.
+#' @param TName Column name of new POSIX timestamp column.
+#' @param tz Timezone (default is "GMT").
+#' @return Data frame with POSIX timestamps.
+#' @export
+#' @examples
+#' data <- readr::read_csv("your_data.csv")
+#' converted_data <- convert_time_to_posix(data, TFormat = "YYYYMMDDHHMM", Year = "Year", Month = "Month", Day = "Day", Hour = "Hour", Min = "Min", TName = "DateTime")
+convert_time_to_posix <- function(data, TFormat, Year, Month, Day, Hour, Min, TName, tz = "GMT") {
+  POSIX_timestamps <- switch(TFormat,
+                             "YYYYMMDDHHMM" = as.POSIXct(paste(data[[Year]], data[[Month]], data[[Day]], data[[Hour]], data[[Min]], sep = "")),
+                             # Add more cases for other time formats if needed
+                             stop("Unsupported time format"))
+  data[[TName]] <- POSIX_timestamps
+  return(data)
+}
+
+#' Initialize XGFrictionData Class
+#'
+#' This function initializes the XGFrictionData class.
+#'
+#' @param data Data frame containing CO2 flux and other relevant variables.
+#' @param metadata Additional metadata.
+#' @return An instance of the XGFrictionData class.
+#' @export
+#' @examples
+#' data <- readr::read_csv("your_data.csv")
+#' metadata <- list(description = "Data for XGFriction package")
+#' xg_friction_data <- initialize_xg_friction_data(data, metadata)
+initialize_xg_friction_data <- function(data, metadata) {
+  setClass("XGFrictionData",
+           slots = c(data = "data.frame", metadata = "list"))
+  new("XGFrictionData", data = data, metadata = metadata)
+}
+
+#' Estimate Ustar Distribution
+#'
+#' This function estimates Ustar thresholds for different quantiles and years.
+#'
+#' @param data Data frame containing CO2 flux and other relevant variables.
 #' @return A list of Ustar thresholds for different quantiles and years.
 #' @export
 #' @examples
 #' data <- readr::read_csv("your_data.csv")
-#' ustar_thresholds <- bootstrap_ustar_thresholds(data)
-bootstrap_ustar_thresholds <- function(data) {
-  # Implement your bootstrapping logic
+#' ustar_thresholds <- estimate_ustar_distribution(data)
+estimate_ustar_distribution <- function(data) {
+  # Implement Ustar distribution estimation logic
   # ...
-
-  return(ustar_thresholds)
 }
-
-
-
-
-
-#' XGBoost Gap Filling
-#'
-#' This function uses XGBoost machine learning to gap-fill CO2 flux data.
-#'
-#' @param data The input data frame containing CO2 flux and other relevant variables.
-#' @return A data frame with gap-filled CO2 flux data.
-#' @export
-#' @examples
-#' data <- readr::read_csv("your_data.csv")
-#' gap_filled_data <- xgboost_gap_fill(data)
-xgboost_gap_fill <- function(data) {
-  # Implement your XGBoost gap filling logic
-  # ...
-
-  return(gap_filled_data)
-}
-
-
-
-
-#' Data Partitioning
-#'
-#' This function partitions the data into nighttime and daytime segments.
-#'
-#' @param data The input data frame containing CO2 flux and other relevant variables.
-#' @return A list with nighttime and daytime segments.
-#' @export
-#' @examples
-#' data <- readr::read_csv("your_data.csv")
-#' partitioned_data <- partition_data(data)
-partition_data <- function(data) {
-  # Implement your data partitioning logic
-  # ...
-
-  return(partitioned_data)
-}
-
 
 
 
